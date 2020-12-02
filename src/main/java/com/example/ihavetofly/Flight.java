1cb38ed8b6bdac23c8da59bc55ec3ff29bdea058
haritsuddin <63796819+haritsuddin@users.noreply.github.com>
2020-12-02 13:38:32 +0700

    Update Flight.java

--- src/main/java/com/example/ihavetofly/Flight.java
+++ src/main/java/com/example/ihavetofly/Flight.java
@@ -13,9 +13,9 @@ public class Flight {
     int toShoot = 0;
     boolean isGoingUp = false;
     int x, y, width, height, wingCounter = 0, shootCounter = 1;
-    Bitmap flight1, flight2, shoot1, shoot2, shoot3, shoot4, shoot5, dead;
-    private GameView gameView;
-
+    Bitmap flight1, flight2, shoot1, shoot2, shoot3, shoot4, shoot5 ;
+    private Gameview gameview;
+    
     Flight (GameView gameView, int screenY, Resources res) {
 
         this.gameView = gameView;
@@ -34,7 +34,7 @@ public class Flight {
 
         flight1 = Bitmap.createScaledBitmap(flight1, width, height, false);
         flight2 = Bitmap.createScaledBitmap(flight2, width, height, false);
-
+        
         shoot1 = BitmapFactory.decodeResource(res, R.drawable.shoot1);
         shoot2 = BitmapFactory.decodeResource(res, R.drawable.shoot2);
         shoot3 = BitmapFactory.decodeResource(res, R.drawable.shoot3);
@@ -47,17 +47,14 @@ public class Flight {
         shoot4 = Bitmap.createScaledBitmap(shoot4, width, height, false);
         shoot5 = Bitmap.createScaledBitmap(shoot5, width, height, false);
 
-        dead = BitmapFactory.decodeResource(res, R.drawable.dead);
-        dead = Bitmap.createScaledBitmap(dead, width, height, false);
-
         y = screenY / 2;
         x = (int) (64 * screenRatioX);
 
     }
 
-    Bitmap getFlight () {
-
-        if (toShoot != 0) {
+    Bitmap getFlight (){
+        
+         if (toShoot != 0) {
 
             if (shootCounter == 1) {
                 shootCounter++;
@@ -86,6 +83,7 @@ public class Flight {
             return shoot5;
         }
 
+          
         if (wingCounter == 0) {
             wingCounter++;
             return flight1;
@@ -94,13 +92,5 @@ public class Flight {
 
         return flight2;
     }
-
-    Rect getCollisionShape () {
-        return new Rect(x, y, x + width, y + height);
-    }
-
-    Bitmap getDead () {
-        return dead;
-    }
-
+    
 }
